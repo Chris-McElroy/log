2adb58a102f39ddf1bc276fc085c85bf8b236fa4
ChrisMcElroy <csmcelroy@me.com>
2023-09-26 12:46:58 -0400

    Initial Commit

---
+++ log/logApp.swift
@@ -0,0 +1,17 @@
+//
+//  logApp.swift
+//  log
+//
+//  Created by 4 on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct logApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
